6a79162ccbe37c51a70e8f8cef9c0dc25ab36370
AsEarth <adarmiracle@gmail.com>
2025-06-04 23:53:29 +0800

    Add files via upload
    Signed-off-by: AsEarth <adarmiracle@gmail.com>

---
+++ freeboom.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>My super super kingdom</title>
+</head>
+<body>
+    <?php echo "I don't know what happened"; ?>
+</body>
+</html>
